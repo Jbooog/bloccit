cb87e9b98cee2b7baf485e4d037f1ed7b627c2eb
Jennie <emomoop@gmail.com>
2014-11-07 22:40:26 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,21 @@ users = User.all
 # to avoid triggering an confirmation email when the User is saved.
 
 # The `save` method then saves this User to the database.
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
 
 
 # Create Posts
 50.times do
   Post.create!(
     user:   users.sample,
+    topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -38,17 +47,41 @@ posts = Post.all
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
-
-User.first.update_attributes!(
-  email: 'emomoop@gmail.com',
-  password: 'jenigump'
-)
+end
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 
-end
+
 
 

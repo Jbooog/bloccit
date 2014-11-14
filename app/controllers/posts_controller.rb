bb52dad03d98c4eebdabc8af247bef717b792683
Jennie <emomoop@gmail.com>
2014-11-14 00:33:08 +0000

    Revised PostsController and TopicsController testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post
 
-    if @post.update_attributes(post_params)
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else

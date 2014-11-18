64dd2e6f77c663cb6efc06c3061b7eb19d83e1b6
Xander Miller <xander.miller@gmail.com>
2014-11-18 23:15:54 +0000

    Fixes various bugs and adds diagnostic gems

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,10 +11,19 @@ class Post < ActiveRecord::Base
    validates :user, presence: true
 
   def markdown_title
-  render_as_markdown title
+    render_as_markdown title
   end
 
   def markdown_body
-  render_as_markdown body
+    render_as_markdown body
+  end
+
+  private
+
+  def render_as_markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
   end
 end

d5788460d6f7917705e4a3365a7de9ab71c2807b
Jennie <emomoop@gmail.com>
2014-11-19 18:40:37 -0800

    Uncommented out Markdown in application helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118183333) do
+ActiveRecord::Schema.define(version: 20141120003912) do
 
   create_table "comments", force: true do |t|
     t.text     "body"

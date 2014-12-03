1eb7b50dc0e7dbc6c046cde1816144ac044ddb1c
Jennie <emomoop@gmail.com>
2014-12-03 11:46:18 -0800

    Destroying

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,10 @@ class CommentsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
+    # this automatically sets @comment.user_id = current_user.id
+    #@comment = Comment.new comment_params
+    #@comment.user_id = current_user.id
+
     @comment.post = @post
 
     if @comment.save
@@ -14,6 +18,21 @@ class CommentsController < ApplicationController
       redirect_to [@topic, @post]
     end
   end
+
+  def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = @topic.posts.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+ 
+     authorize @comment
+     if @comment.destroy
+       flash[:notice] = "Comment was removed."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "Comment couldn't be deleted. Try again."
+       redirect_to [@topic, @post]
+     end
+   end
   private
 
 def comment_params

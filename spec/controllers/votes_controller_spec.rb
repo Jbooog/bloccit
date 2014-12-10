dae9d07da9e4963e852e170a2a969082a4d43303
Jennie <emomoop@gmail.com>
2014-12-10 14:24:46 -0800

    #down_vote controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -10,7 +10,8 @@ require 'rails_helper'
     @user = authenticated_user
     @post = associated_post
     sign_in @user
-  end       
+  end 
+        
   describe '#up_vote' do
      it "adds an up-vote to the post" do
      expect { post( :up_vote, post_id: @post.id )}.to change{ @post.up_votes }.by 1

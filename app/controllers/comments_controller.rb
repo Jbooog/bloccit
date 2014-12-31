19c8bbc45d4198e2e997489ef44e347980cef55d
Jennie <emomoop@gmail.com>
2014-12-31 08:30:36 -0800

    Destroying with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
   
     def create 
      @post = Post.find(params[:post_id])
@@ -15,7 +16,6 @@ class CommentsController < ApplicationController
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
     end
   end
 
@@ -30,7 +30,11 @@ class CommentsController < ApplicationController
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
      end
-     redirect_to [@topic, @post]
+   
+
+   respond_with(@comment) do |format|
+       format.html { redirect_to [@post.topic, @post] }
+     end
    end
   private
 
